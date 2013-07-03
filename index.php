631663ae4f25d09d64b0f469a3e3c3b0648f05ad
alex <am3081@gmail.com>
2013-07-03 12:50:10 -0400

    login and admin panel

--- index.php
+++ index.php
@@ -1,14 +1,48 @@
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!doctype html>
+<html lang="en" ng-app="myApp">
     <head>
-        <title>AVAIL Transit Demand Modeling</title>
+        <meta charset="utf-8">
+        <title>AVAIL TDM Admin - NJTransit</title>
+        <link rel="stylesheet" href="admin/css/app.css"/>
+        <link rel="stylesheet" href="admin/css/custom.css"/>
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements. All other JS at the end of file. -->
+    <!--[if lt IE 9]>
+      <script src="js/lib/html5shiv.js"></script>
+    <![endif]-->
+  
     </head>
-    <body>
-        Welcome to Avail Transit Demand Modeling.<br>
-        <a href='zone_select/'>Zone Select</a><br>
-        <a href='model/?zone=1'>Patterson Market</a><br>
-        <a href='model/?zone=2'>Atlantic City Market</a><br>
-        <a href='model/?zone=3'>Greater Philadelphia Market Area</a><br>
-        <a href='model/?zone=4'>Princeton Model</a><br>
-        <a href='model/?zone=5'>Newark Model</a><br>
-    </body>
+    <body class='dark'> 
+        <div class="container login-container">
+            <div class="row login-row">
+                <div class="span1 offset3">
+                  <div class="login-icon">
+                    <img src="admin/img/Login-Icon-Map.png" alt="Welcome to Mail App">
+                    <div class="login-text">AVAIL /<br>NJTransit</div>
+                        <div class='small-text'>
+                        Tranist<br>
+                        Demand<br>
+                        Modeling
+                        </div>
+                  </div>
+                </div>
+                <div class="span4">
+                    <div class="login-form">
+                        <div class="control-group">
+                          <input type="text" class="login-field" value="" placeholder="User" id="login-name">
+                          <label class="login-field-icon fui-user" for="login-name"></label>
+                        </div>
+
+                        <div class="control-group">
+                          <input type="password" class="login-field" value="" placeholder="Password" id="login-pass">
+                          <label class="login-field-icon fui-lock" for="login-pass"></label>
+                        </div>
+
+                        <a class="btn btn-inverse btn-large btn-block" href="#">Login</a>
+                        <!-- <a class="login-link" href="#">Lost your password?</a> -->
+                    </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+</body>
 </html>
